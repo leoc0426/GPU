f2c518d46e8eba0cb1e54c0bb17e39d2888aa61d
YongYuH <YongYuH@users.noreply.github.com>
2015-11-28 18:48:22 +0800

    Create viz_result.m
    visualize the Rusanov-Flux 1D shock tube results

---
+++ viz_result.m
@@ -0,0 +1,25 @@
+Sodtube_Rusanov_Ryan_Results = load('Sodtube_Rusanov_Ryan_Results.txt');
+
+x = linspace(1, 200, 200);
+
+figure(1);
+set(gcf, 'Position', get(0,'ScreenSize'));
+
+for index = 1:1:300    
+    subplot(3,1,1);
+    plot(x, Sodtube_Rusanov_Results((index-1)*200+1:(index)*200,1))
+    title(['Temperature'])
+    axis([1 200 -2 2])
+
+    subplot(3,1,2);
+    plot(x, Sodtube_Rusanov_Results((index-1)*200+1:(index)*200,2))
+    title(['Velocity'])
+    axis([1 200 -1 1])
+
+    subplot(3,1,3);
+    plot(x, Sodtube_Rusanov_Results((index-1)*200+1:(index)*200,3))
+    title(['Pressure'])
+    axis([1 200 0 1])
+    
+    getframe
+end
