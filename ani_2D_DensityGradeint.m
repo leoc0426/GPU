85e90948d9271b2070ccc10728966cebeed4cf6e
YongYuH <larry830326@hotmail.com>
2015-12-10 14:16:26 +0800

    Create the matlab code to animate the density gradient change of shock bubble

---
+++ ani_2D_DensityGradeint.m
@@ -0,0 +1,21 @@
+clear; clc;
+A = load('2DResults.txt');
+
+X = reshape(A(1:100*100,1), 100, 100);
+Y = reshape(A(1:100*100,2), 100, 100);
+
+figure(1)
+set(gcf, 'Position', get(0,'ScreenSize'));
+
+for i=1:1:100
+    D = reshape(A((100*100)*(i-1)+1:100*100*i,3), 100, 100);
+    [FX,FY] = gradient(D,1,1);
+    pcolor(X,Y,sqrt(FX.^2+FY.^2));
+    shading flat;
+    colormap(flipud(gray));
+    caxis([0, 0.35]); 
+    view(2);
+    axis equal;
+    title(['Density distribution']);
+    getframe;
+end
