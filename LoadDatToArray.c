3de07cefd4c044463005d56132fe97215a9dad2c
YongYuH <YongYuH@users.noreply.github.com>
2016-01-06 15:59:46 +0800

    Update LoadDatToArray.c
    Use fscanf to replace strtok

--- LoadDatToArray.c
+++ LoadDatToArray.c
@@ -2,9 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define DEPTH  100			// total grids in x direction
-#define WIDTH  100			// total grids in y direction
-#define HEIGHT 100			// total grids in z direction
+#define DEPTH  100								// total grids in x direction
+#define WIDTH  100								// total grids in y direction
+#define HEIGHT 100								// total grids in z direction
 
 void InitBody(float* body);
 void FileToArray(char* filename, float* body);
@@ -23,15 +23,7 @@ int main () {
 
 
 void InitBody(float* body) {
-	int x, y, z, idx;
-	for(x = 0; x < DEPTH; x++) {
-    		for(y = 0; y < WIDTH; y++) {
-    			for(z = 0; z < HEIGHT; z++) {
-				idx = x*WIDTH*HEIGHT + y*HEIGHT + z;
-				body[idx] = 0.0;
-			}
-		}
-	}
+	body[DEPTH*WIDTH*HEIGHT] = 0.0;
 }
 
 
@@ -39,30 +31,26 @@ void FileToArray(char* filename, float* body) {
 	FILE *pFile;
 	pFile = fopen(filename, "r");
 	if (!pFile) { printf("Open failure"); } 
-
+    
 	char line[60];
-
+     
 	int x, y, z, idx;
+	float tmp1, tmp2, tmp3;
     
 	for(x = 25; x < 75; x++) {
 		for(y = 25; y < 75; y++) {
 			for(z = 25; z < 75; z++) {
 				idx = x*50*50 + y*50 + z;
-    			
-				fgets(line, sizeof(line), pFile);
-				char *value = strtok(line, " ");
-     
-				while (value != NULL) {			    	
-					body[idx] = atof(value);
-					/* test... */
-					// 0.040018	 -0.204846	 -0.286759	 -1
-					//printf("%f ", body[idx]);
-					value = strtok(NULL, " ");
-				}
-				/* test... */
-				//system("pause");    
+ 
+				while (fgets(line, sizeof(line), pFile) != NULL) {	
+					fscanf(pFile, "%f%f%f%f", &tmp1, &tmp2, &tmp3, &body[idx]);		    	
+					/* test... 0.040018	 -0.204846	 -0.286759	 -1 */		        	
+					//printf("%f\n", body[idx]);
+					//if(body[idx] == 0) { system("pause"); }
+				}		    	    
 			}
 		}
-    	}
+	}
 	fclose(pFile); 
 }
+
