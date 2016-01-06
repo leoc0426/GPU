d9c6f04ce1a6ece3eb92c04ac3f96d05628b8139
YongYuH <YongYuH@users.noreply.github.com>
2016-01-06 14:52:07 +0800

    Update LoadDatToArray.c
    change the typewriting

--- LoadDatToArray.c
+++ LoadDatToArray.c
@@ -2,9 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define DEPTH  100								// total grids in x direction
-#define WIDTH  100								// total grids in y direction
-#define HEIGHT 100								// total grids in z direction
+#define DEPTH  100			// total grids in x direction
+#define WIDTH  100			// total grids in y direction
+#define HEIGHT 100			// total grids in z direction
 
 void InitBody(float* body);
 void FileToArray(char* filename, float* body);
@@ -27,8 +27,8 @@ void InitBody(float* body) {
 	for(x = 0; x < DEPTH; x++) {
     		for(y = 0; y < WIDTH; y++) {
     			for(z = 0; z < HEIGHT; z++) {
-    			idx = x*WIDTH*HEIGHT + y*HEIGHT + z;
-    			body[idx] = 0.0;
+				idx = x*WIDTH*HEIGHT + y*HEIGHT + z;
+				body[idx] = 0.0;
 			}
 		}
 	}
@@ -39,25 +39,25 @@ void FileToArray(char* filename, float* body) {
 	FILE *pFile;
 	pFile = fopen(filename, "r");
 	if (!pFile) { printf("Open failure"); } 
-    
+
 	char line[60];
-     
+
 	int x, y, z, idx;
     
 	for(x = 25; x < 75; x++) {
 		for(y = 25; y < 75; y++) {
 			for(z = 25; z < 75; z++) {
-    				idx = x*50*50 + y*50 + z;
+				idx = x*50*50 + y*50 + z;
     			
-    				fgets(line, sizeof(line), pFile);
-    				char *value = strtok(line, " ");
+				fgets(line, sizeof(line), pFile);
+				char *value = strtok(line, " ");
      
 				while (value != NULL) {			    	
-		        	body[idx] = atof(value);
-		        	/* test... */
-		        	// 0.040018	 -0.204846	 -0.286759	 -1
-		        	//printf("%f ", body[idx]);
-			        value = strtok(NULL, " ");
+					body[idx] = atof(value);
+					/* test... */
+					// 0.040018	 -0.204846	 -0.286759	 -1
+					//printf("%f ", body[idx]);
+					value = strtok(NULL, " ");
 				}
 				/* test... */
 				//system("pause");    
