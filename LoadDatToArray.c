a384347504123463a41cc520516622cb22301c01
YongYuH <YongYuH@users.noreply.github.com>
2016-01-06 14:46:07 +0800

    Update LoadDatToArray.c
    change the typewriting

--- LoadDatToArray.c
+++ LoadDatToArray.c
@@ -25,45 +25,44 @@ int main () {
 void InitBody(float* body) {
 	int x, y, z, idx;
 	for(x = 0; x < DEPTH; x++) {
-    	for(y = 0; y < WIDTH; y++) {
-    		for(z = 0; z < HEIGHT; z++) {
+    		for(y = 0; y < WIDTH; y++) {
+    			for(z = 0; z < HEIGHT; z++) {
     			idx = x*WIDTH*HEIGHT + y*HEIGHT + z;
     			body[idx] = 0.0;
 			}
 		}
-    }
+	}
 }
 
 
 void FileToArray(char* filename, float* body) {
 	FILE *pFile;
 	pFile = fopen(filename, "r");
-    if (!pFile) { printf("Open failure"); } 
+	if (!pFile) { printf("Open failure"); } 
     
-    char line[60];
+	char line[60];
      
-    int x, y, z, idx;
+	int x, y, z, idx;
     
-    for(x = 25; x < 75; x++) {
-    	for(y = 25; y < 75; y++) {
-    		for(z = 25; z < 75; z++) {
-    			idx = x*50*50 + y*50 + z;
+	for(x = 25; x < 75; x++) {
+		for(y = 25; y < 75; y++) {
+			for(z = 25; z < 75; z++) {
+    				idx = x*50*50 + y*50 + z;
     			
-    			fgets(line, sizeof(line), pFile);
-    			char *value = strtok(line, " ");
+    				fgets(line, sizeof(line), pFile);
+    				char *value = strtok(line, " ");
      
-			    while (value != NULL) {			    	
+				while (value != NULL) {			    	
 		        	body[idx] = atof(value);
 		        	/* test... */
 		        	// 0.040018	 -0.204846	 -0.286759	 -1
 		        	//printf("%f ", body[idx]);
 			        value = strtok(NULL, " ");
-			    }
-			    /* test... */
-		    	//system("pause");    
+				}
+				/* test... */
+				//system("pause");    
 			}
 		}
-    }
-    fclose(pFile); 
+    	}
+	fclose(pFile); 
 }
-
